0e4f2b316c0e26caa0b7a23bef455d9c3f8298e7
Juan Carlos <juancarlospaco@gmail.com>
2024-11-11 10:11:37 -0300

    Style

--- test.nim
+++ test.nim
@@ -1,4 +1,5 @@
-import std/[json, envvars]
+import std/[envvars, json]
+
 import src/client
 
 
@@ -16,28 +17,30 @@ proc broadcast_callback(payload: JsonNode) =
   echo "broadcast: ", payload
 
 
-var
-  url = getEnv("SUPABASE_URL")
-  key = getEnv("SUPABASE_KEY")
-var rclient = newRealtimeClient(url, key, auto_reconnect = true)
-var
-  chan = rclient.setChannel("pg-test", broadcast_config)
-  chan2 = rclient.setChannel("broadcast_test", broadcast_config)
+proc main() =
+  let
+    url = getEnv("SUPABASE_URL")
+    key = getEnv("SUPABASE_KEY")
+    rclient = newRealtimeClient(url, key, auto_reconnect = true)
+  var
+    chan  = rclient.setChannel("pg-test", broadcast_config)
+    chan2 = rclient.setChannel("broadcast_test", broadcast_config)
+  defer: rclient.close()
+
+  rclient.join(chan)
+  chan.on_postgres_changes(PostgresChanges.INSERT, postgres_changes_callback, filter="item=eq.10")
+  chan.on_postgres_changes(PostgresChanges.DELETE, delete_callback)
+  rclient.subscribe(chan)
+
+  rclient.join(chan2)
+  chan2.on_broadcast("event_test", broadcast_callback)
+  rclient.subscribe(chan2)
+
+  for i in 0 .. 10:
+    rclient.send_broadcast(chan2, "event_test", %*{"data": "pong - " & $i})
+
+  rclient.listen()
 
 
-rclient.join(chan)
-chan.on_postgres_changes(PostgresChanges.INSERT, postgres_changes_callback, filter = "item=eq.10")
-chan.on_postgres_changes(PostgresChanges.DELETE, delete_callback)
-rclient.subscribe(chan)
-
-
-rclient.join(chan2)
-chan2.on_broadcast("event_test", broadcast_callback)
-rclient.subscribe(chan2)
-
-
-for i in 0 .. 10:
-  rclient.send_broadcast(chan2, "event_test", %*{"data": "pong - " & $i})
-
-
-rclient.listen()
+when isMainModule:
+  main()
